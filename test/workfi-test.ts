6c132d28850456b49108fb6595cd8495f44802ae
Anthalasath <chapchico@live.com>
2022-07-14 23:02:11 +0200

    test for changing workers

--- test/workfi-test.ts
+++ test/workfi-test.ts
@@ -100,5 +100,48 @@ describe("WorkFi", function () {
       expect(bounty.deadline).to.eq(deadline);
       expect(bounty.hasWorkerBeenPaid).to.be.false;
    });
+
+   it('changes the worker when calling acceptWorker if there is already a worker. Only the worker address is changed.', async () => {
+      const MockErc20 = await hre.ethers.getContractFactory('MockERC20');
+      const nativeToken = await MockErc20.deploy('NativeToken', 'NT');
+      await nativeToken.deployed();
+      const stablecoin = await MockErc20.deploy('StableCoin', 'SC');
+      await stablecoin.deployed();
+      const WorkFi = await hre.ethers.getContractFactory('WorkFi');
+      const workFi = await WorkFi.deploy(stablecoin.address);
+      await (await nativeToken.approve(workFi.address, hre.ethers.utils.parseEther('100'))).wait();
+      const stablePay = hre.ethers.utils.parseEther('0');
+      const nativePay = hre.ethers.utils.parseEther('1');
+      const exchangeRate = hre.ethers.utils.parseEther('2');
+      const blockNumber = await hre.ethers.provider.getBlockNumber();
+      const block = await hre.ethers.provider.getBlock(blockNumber);
+      const deadline = block.timestamp + hours(1);
+      const [signerAddress, firstWorkerAddress, secondWorkerAddress] = await Promise.all((await hre.ethers.getSigners()).map(s => s.getAddress()));
+
+      await (await workFi.createBounty(
+         stablePay,
+         nativePay,
+         exchangeRate,
+         nativeToken.address,
+         deadline
+      )).wait();
+
+      await (await workFi.acceptWorker(1, firstWorkerAddress)).wait();
+      await (await workFi.acceptWorker(1, secondWorkerAddress)).wait();
+
+      const filter = workFi.filters.WorkerAccepted(1, secondWorkerAddress);
+      const events = await workFi.queryFilter(filter);
+      expect(events.length).to.eq(1);
+      const bounty = await workFi.getBounty(events[0].args.bountyId);
+      expect(bounty.stablePay).to.eq(stablePay);
+      expect(bounty.nativePay).to.eq(nativePay);
+      expect(bounty.exchangeRate).to.eq(exchangeRate);
+      expect(bounty.nativeToken).to.eq(nativeToken.address);
+      expect(bounty.worker).to.eq(secondWorkerAddress); 
+      expect(bounty.recruiter).to.eq(signerAddress);
+      expect(bounty.isCompleted).to.be.false;
+      expect(bounty.deadline).to.eq(deadline);
+      expect(bounty.hasWorkerBeenPaid).to.be.false;
+   });
 });
 
